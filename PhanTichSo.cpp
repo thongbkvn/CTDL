044db6d3d0573b91086ab36c4d5ccda2dda0aa01
Pham Van Thong <thongbkvn@gmail.com>
2015-01-26 00:27:29 +0700

    Phan tich so thanh tong

--- PhanTichSo.cpp
+++ PhanTichSo.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 
-#define S 6 //Phan tich S thanh tong cac so
+#define S 9 //Phan tich S thanh tong cac so
 
-int A[S+1],T[S+1];
+int A[S+1]; //Luu cac so trong chuoi tong
+int T[S+1]; //T[i] Luu tong cac so tu A[1] den A[i]
+
+//Ham in ket qua
 void inkq(int n)
 {
   int i;
@@ -12,18 +15,19 @@ void inkq(int n)
   cout<<A[n]<<endl;
 }
 
+//Ham phan tich tong theo phuong phap quay lui
 void Try(int k=1)
 {
   int i;
-  for (i=A[k-1];i<S;i++)
+  for (i=A[k-1];i<=S-T[k-1];i++) //Moi gia tri co the thu
     {
-      A[k]=i;
-      T[k]=T[k-1]+i;
-      if (T[k]==S)
+      A[k]=i; //Thu cho A[k]=i
+      T[k]=T[k-1]+i;  //Cap nhat tong A[k]
+      if (T[k]==S) //Neu thoa man thi in ket qua
 	inkq(k);
-      else
+      else //Neu chua phai la ket qua
 	{
-	  if (T[k-1]<S)
+	  if (T[k-1]<S) //Neu van con kha nang tim ra ket qua thi thu tiep
 	    Try(k+1);
 	}	   	
     }
